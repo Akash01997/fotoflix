81696e9562c10888653cdd9b69c6ee120d4bc6e0
Akash01997 <akashshinde558@gmail.com>
2024-06-10 14:31:19 +0530

    Photos fetching from API

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -3,10 +3,63 @@ import { useState, useEffect } from 'react';
 import {FaHeart, FaDownload, FaShare} from 'react-icons/fa';
 
 const Photos = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(()=>{
+        const fetchImage = async ()=>{
+            setLoading(true);
+            const clientID= '?client_id=JhyJwBR1kF1u34YmgH2HGdxowgmMXOSeq1m3WpP6nOA';
+            const mainUrl = 'https://api.unsplash.com/photos/';
+            try {
+                const response = await fetch (`${mainUrl}${clientID}`)
+                const data =  await response.json();
+                setPhotos(data)
+                setLoading(false)
+            }
+            catch(error){
+                setLoading(false);
+                console.log(error);
+            }
+        };
+        fetchImage();
+    },[]);
+
   return (
-    <div>
-      
-    </div>
+    <main>
+        <section className='photos'>
+            {loading ? (<p>
+                Loading.....</p>
+                ):(
+                    photos.map((photo)=>(
+                <article key={photo.id} className='photo'>
+                    <img src={photo.urls.regular} alt={photo.alt_description} />
+                    <div className="photo info">
+                        <div className="photo-header">
+                            <h4>{photo.user.name}</h4>
+                            <button className='favourite-btn'> <FaHeart /></button>
+                        </div>
+                        <div className="photo-action">
+                            <p>
+                                <FaHeart className='heart-icon' />{photo.likes}
+                            </p>
+                            <button className='share-btn'>
+                                <FaShare />
+                            </button>
+                            <button className="download-btn">
+                                <FaDownload />
+                            </button>
+                        </div>
+                        <a href={photo.user.portfolio_url}>
+                            <img src={photo.user.profile_image.medium} className='user-img' alt={photo.user.name} />
+                        </a>
+                    </div>
+                </article>
+  ))
+  )}
+        </section>
+    </main>
   )
 }
 
