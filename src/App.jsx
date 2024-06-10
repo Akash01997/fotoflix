55948457545f35b27b1e6c92ebc70afcb9f6c2fa
Akash01997 <akashshinde558@gmail.com>
2024-06-10 13:50:04 +0530

    Router Setup _ FA installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Photos from './components/Photos';
+import Favourites from './components/Favourites';
+import { FaSearch } from 'react-icons/fa';
+
+
 
 function App() {
 
   return (
     <>
-      <h1>welcome</h1>
+      <BrowserRouter>
+        <div>
+          <nav className="navbar">
+            <div className="navbar_logo">   
+              FOTOFLIX
+            </div>
+            <form action='' className='navbar_search-form'>
+              <input type='text' className='form-input' placeholder='search'/>
+              <button type='submit' className='submit-btn'>
+                <FaSearch />
+              </button>
+            </form>
+            <div className="navbar_links">
+              <Link to="/">Home</Link>
+              <Link to="/Favourites">Favourites</Link>
+            </div>
+          </nav>
+          <Routes>
+            <Route path='/' element={<Photos />}/>
+            <Route path='/Favourites' element={<Favourites />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Photos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {FaHeart, FaDownload, FaShare} from 'react-icons/fa';
+
+const Photos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Photos
